afb7b8f4b4dc22816a20cd29ead8176204a545bc
Volodymyr Zatkhey <Volodymyr.Zatkhey@lifechemicals.com>
2020-07-10 13:22:58 +0300

    before connect github

---
+++ Scripts/За результатами наради/cLogP_analyses.sql
@@ -0,0 +1,20 @@
+use Chest35;
+go
+
+select round(cLogP, 1) as rnd_cLogP, COUNT(*) as cnt 
+from MaterialCalculatedParams
+group by round(cLogP, 1)
+order by round(cLogP, 1) ;
+
+select round(cLogP, 0) as rnd_cLogP, COUNT(*) as cnt 
+from MaterialCalculatedParams
+where round(cLogP, 0) between -2 and 6
+group by round(cLogP, 0)
+order by round(cLogP, 0) ;
+
+select  m.MatName as ID, mcp.cLogP
+from MaterialCalculatedParams mcp
+	inner join Materials m on m.MatID = mcp.MatID
+where mcp.cLogP >= 10 or mcp.cLogP = 0
+order by mcp.cLogP
+;
