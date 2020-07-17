c13beaa3c86635090390c86d127b0abbe3184a1e
Volodymyr Zatkhey <Volodymyr.Zatkhey@lifechemicals.com>
2020-07-17 11:37:29 +0300

    add rest SpInfo >50 g

---
+++ Scripts/Залишки СпектрумІнфо.sql
@@ -0,0 +1,43 @@
+/*
+Володя, добрый день, сделай мне, пожалуйста, выборку по всем продуктам Спектрум, которых имеется больше 50г. 
+Критерий – поставки должны быть от BSB, SUA, PSV, ADA, JTG, OAM, GER, VAP, MSO, DAO.
+
+Спасибо,
+Сергей Попов
+*/
+use Chest35;
+go
+
+declare @ChemList as table(Abbr char(3));
+insert into @ChemList values
+('BSB')
+,('SUA')
+,('PSV')
+,('ADA')
+,('JTG')
+,('OAM')
+,('GER')
+,('VAP')
+,('MSO')
+,('DAO')
+--,('SKP')
+--,('MNB')
+--,('DEU')
+--,('MEM')
+--, ('MLG')
+--, ('DNF')
+--, ('BEV')
+--, ('KEV')
+
+--select Abbr from @ChemList;
+
+select p.Код_поставщика
+	, p.ID 
+	, p.Код_поставки
+	, p.Масса_пост
+	--, * 
+from tblПоставки p
+	inner join @ChemList cl on p.Код_поставщика = cl.Abbr
+where [Масса_пост]>=50000
+order by p.Код_поставщика
+	, p.ID;
