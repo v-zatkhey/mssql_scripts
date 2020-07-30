fe4796d8569aa55c5c54c5b2185ebdf6a640740f
Volodymyr Zatkhey <Volodymyr.Zatkhey@lifechemicals.com>
2020-07-30 22:10:56 +0300

    trigger and rests on SKLAD

--- ClientRequest/ClientRequests_trigger_UPD.sql
+++ ClientRequest/ClientRequests_trigger_UPD.sql
@@ -7,18 +7,22 @@ GO
 -- Create date: 29.07.2020
 -- Description:	update default fields
 -- =============================================
-CREATE TRIGGER dbo.TR_tblClientRequests_UPD 
+--CREATE TRIGGER dbo.TR_tblClientRequests_UPD 
+ALTER TRIGGER dbo.TR_tblClientRequests_UPD 
    ON  dbo.tblClientRequests 
    AFTER UPDATE
 AS 
 BEGIN
 	SET NOCOUNT ON;
 	declare @UserID int;
+	select @UserID = (select top 1 Код from dbo.tblПользователи where SYSTEM_USER = 'IFLAB\'+ WinLogin);
 	
     if update(AnswerText)
     begin
 		update c
-		set HasAnswer = 1
+		set HasAnswer = 1,
+			AnswerEmployee = @UserID
+			,AnswerDate = GETDATE()
 		from tblClientRequests c 
 			inner join deleted d on d.ID = c.ID
 		where d.HasAnswer = 0;
@@ -26,24 +30,15 @@ BEGIN
 
     if update(HasAnswer)
     begin
-		-- add_answer_id
-		select @UserID = (select top 1 Код from dbo.tblПользователи where SYSTEM_USER = 'IFLAB\'+ WinLogin);
-		
+    
+		-- add_answer_id and timestamp
 		update x
 		set AnswerEmployee = @UserID
+			,AnswerDate = GETDATE()
 		from tblClientRequests x
 			inner join inserted i on i.ID = x.ID
 			inner join deleted d on d.ID = x.ID
 		where d.HasAnswer = 0 and i.HasAnswer = 1;  
-		
-		-- timestamp
-		update x
-		set AnswerDate = GETDATE()
-		from tblClientRequests x
-			inner join inserted i on i.ID = x.ID
-			inner join deleted d on i.ID = x.ID
-		where d.HasAnswer = 0 and i.HasAnswer = 1;  
-		
     end;
 
 END
